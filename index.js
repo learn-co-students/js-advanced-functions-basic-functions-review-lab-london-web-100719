bb5096c8ee6e8633c357751174b560124c1920be
Tish Chungoora <tishal82@gmail.com>
2019-11-27 17:16:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(myParam="*") {
+    return function(adjective="special") {
+      return `You are ${myParam}${adjective}${myParam}!`
+    }
+  }
+
+let Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b
+    },
+    multiply: function(a,b) {
+        return a * b
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+
+let actionApplyer = function(startInt, funcArr) {
+    let start = startInt
+
+    for (let i = 0; i < funcArr.length; i++) {
+        start = funcArr[i](start)
+    } 
+    return start
+}
