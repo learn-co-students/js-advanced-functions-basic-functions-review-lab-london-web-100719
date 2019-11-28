01e1e860ccaee18e30a0944af7a85738de4245ad
cseiasp <caroline.seilern@hotmail.com>
2019-11-28 10:11:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(another = "*"){
+    return function(arg = "special"){
+        return `You are ${another + arg + another}!`
+    }
+}
+
+const Calculator = {
+    add,
+    subtract,
+    multiply,
+    divide
+} 
+
+function add(a,b){
+return a + b
+}
+
+function subtract(a,b){
+return a - b
+}
+
+function multiply(a,b){
+return a * b
+}
+
+function divide(a,b){
+return a / b
+}
+
+function actionApplyer(startingInt, fnArray){
+    let handledInt = startingInt
+    fnArray.forEach( (el) => {
+        handledInt = el(handledInt)
+    })
+    
+    return handledInt
+}
